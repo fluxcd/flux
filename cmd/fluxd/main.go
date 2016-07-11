690c5be5e445c63bbeaba71d817c49834566cc6a
Michael Bridgen <mikeb@squaremobius.net>
2016-07-11 12:31:24 +0100

    Stub of HTTP-based API server

--- cmd/fluxd/main.go
+++ cmd/fluxd/main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"net"
 	"os"
 
 	"github.com/go-kit/kit/log"
 	"github.com/spf13/cobra"
+
+	"github.com/weaveworks/fluxy/api"
 )
 
 func main() {
@@ -32,35 +32,19 @@ func main() {
 type daemonOpts struct {
 	logger     log.Logger
 	listenAddr string
+	credsPath  string
 }
 
 func (opts *daemonOpts) addFlags(cmd *cobra.Command) {
 	cmd.Flags().StringVarP(&opts.listenAddr, "listen", "l", ":3030", "the address to listen for flux API clients on")
+	cmd.Flags().StringVar(&opts.credsPath, "credentials", "", "path to a credentials file a la Docker, e.g., mounted as a secret")
 }
 
 func (opts *daemonOpts) run(cmd *cobra.Command, args []string) error {
 	if len(args) > 0 {
 		return fmt.Errorf("did not expect any arguments")
 	}
-	srv, err := net.Listen("tcp", opts.listenAddr)
-	if err != nil {
-		return err
-	}
-	opts.logger.Log("addr", opts.listenAddr, "msg", "listening")
-	for {
-		conn, err := srv.Accept()
-		if err != nil {
-			opts.logger.Log("err", err)
-		}
-		go echo(conn, opts.logger)
-	}
-}
-
-func echo(conn net.Conn, logger log.Logger) {
-	logger.Log("addr", conn.RemoteAddr(), "msg", "accepted")
-	defer logger.Log("addr", conn.RemoteAddr(), "msg", "closed")
-	lines := bufio.NewScanner(conn)
-	for lines.Scan() {
-		conn.Write([]byte(lines.Text() + "\n")) // Scan strips newline
-	}
+	apisrv := api.APIServer{}
+	opts.logger.Log("listen-err", apisrv.ListenAndServe(opts.listenAddr))
+	return nil
 }
