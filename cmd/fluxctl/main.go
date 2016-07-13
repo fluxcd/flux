778ff68255fda2e553867ba5bd215826b173bab9
Peter Bourgon <peter@bourgon.org>
2016-07-13 18:06:14 +0200

    cmd/fluxctl: first draft, listing services

--- cmd/fluxctl/main.go
+++ cmd/fluxctl/main.go
@@ -1,9 +1,64 @@
 package main
 
 import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+
 	"github.com/spf13/cobra"
 )
 
+// These are the (unfortunately global) flags.
+var (
+	OptionURL       string
+	OptionNamespace string
+)
+
 func main() {
-	_ = cobra.Command{}
+	rootCmd := &cobra.Command{
+		Use:          "fluxctl",
+		Short:        "fluxctl is a commandline client for the fluxd daemon.",
+		SilenceUsage: true,
+	}
+	rootCmd.Flags().StringVarP(&OptionURL, "url", "u", "http://localhost:3030/v0/", "Base URL of the fluxd API server")
+
+	serviceCmd := &cobra.Command{
+		Use:   "service <list, ...> [options]",
+		Short: "Manipulate platform services.",
+	}
+
+	serviceListCmd := &cobra.Command{
+		Use:   "list",
+		Short: "List services currently running on the platform.",
+		RunE:  serviceList,
+	}
+	serviceListCmd.Flags().StringVarP(&OptionNamespace, "namespace", "n", "default", "Namespace to introspect")
+
+	rootCmd.AddCommand(serviceCmd)
+	serviceCmd.AddCommand(serviceListCmd)
+
+	if err := rootCmd.Execute(); err != nil {
+		os.Exit(1)
+	}
+}
+
+func serviceList(cmd *cobra.Command, args []string) error {
+	req, err := http.NewRequest("GET", fmt.Sprintf(
+		"%s/services?namespace=%s",
+		OptionURL,
+		OptionNamespace,
+	), nil)
+	if err != nil {
+		return err
+	}
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return err
+	}
+
+	io.Copy(os.Stdout, resp.Body)
+	resp.Body.Close()
+	return nil
 }
