90d0d5ffbe64fd0282dadfacf3e497775ec995e1
Peter Bourgon <peter@bourgon.org>
2016-07-13 19:26:11 +0200

    cmd/fluxctl: fluxctl service release

--- cmd/fluxctl/main.go
+++ cmd/fluxctl/main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"bytes"
+	"errors"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"net/http"
+	"net/url"
 	"os"
+	"time"
 
 	"github.com/spf13/cobra"
 )
@@ -16,7 +21,7 @@ func main() {
 		Short:        "fluxctl is a commandline client for the fluxd daemon.",
 		SilenceUsage: true,
 	}
-	rootCmd.PersistentFlags().StringVarP(&rootOpts.URL, "url", "u", "http://localhost:3030/v0/", "base URL of the fluxd API server")
+	rootCmd.PersistentFlags().StringVarP(&rootOpts.URL, "url", "u", "http://localhost:3030/v0", "base URL of the fluxd API server")
 
 	serviceCmd := &cobra.Command{
 		Use:   "service <list, ...> [options]",
@@ -31,8 +36,20 @@ func main() {
 	}
 	serviceListCmd.Flags().StringVarP(&serviceListOpts.Namespace, "namespace", "n", "default", "namespace to introspect")
 
+	serviceReleaseOpts := &serviceReleaseOpts{rootOpts: rootOpts}
+	serviceReleaseCmd := &cobra.Command{
+		Use:   "release",
+		Short: "Release a new version of a service.",
+		RunE:  serviceReleaseOpts.RunE,
+	}
+	serviceReleaseCmd.Flags().StringVarP(&serviceReleaseOpts.Namespace, "namespace", "n", "default", "namespace to introspect")
+	serviceReleaseCmd.Flags().StringVarP(&serviceReleaseOpts.Service, "service", "s", "", "service to update")
+	serviceReleaseCmd.Flags().StringVarP(&serviceReleaseOpts.File, "file", "f", "-", "file containing new ReplicationController definition, or - to read from stdin")
+	serviceReleaseCmd.Flags().DurationVarP(&serviceReleaseOpts.UpdatePeriod, "update-period", "p", 5*time.Second, "delay between starting and stopping instances in the rolling update")
+
 	rootCmd.AddCommand(serviceCmd)
 	serviceCmd.AddCommand(serviceListCmd)
+	serviceCmd.AddCommand(serviceReleaseCmd)
 
 	if err := rootCmd.Execute(); err != nil {
 		os.Exit(1)
@@ -52,7 +69,7 @@ func (opts *serviceListOpts) RunE(*cobra.Command, []string) error {
 	req, err := http.NewRequest("GET", fmt.Sprintf(
 		"%s/services?namespace=%s",
 		opts.URL,
-		opts.Namespace,
+		url.QueryEscape(opts.Namespace),
 	), nil)
 	if err != nil {
 		return err
@@ -62,8 +79,72 @@ func (opts *serviceListOpts) RunE(*cobra.Command, []string) error {
 	if err != nil {
 		return err
 	}
+	defer resp.Body.Close()
 
 	io.Copy(os.Stdout, resp.Body)
-	resp.Body.Close()
+	return nil
+}
+
+type serviceReleaseOpts struct {
+	*rootOpts
+	Namespace    string
+	Service      string
+	File         string
+	UpdatePeriod time.Duration
+}
+
+func (opts *serviceReleaseOpts) RunE(*cobra.Command, []string) error {
+	if opts.Service == "" {
+		return errors.New("-s, --service is required")
+	}
+
+	var buf []byte
+	var err error
+	switch opts.File {
+	case "":
+		return errors.New("-f, --file is required")
+
+	case "-":
+		buf, err = ioutil.ReadAll(os.Stdin)
+		if err != nil {
+			return err
+		}
+
+	default:
+		buf, err = ioutil.ReadFile(opts.File)
+		if err != nil {
+			return err
+		}
+	}
+
+	req, err := http.NewRequest("POST", fmt.Sprintf(
+		"%s/release?namespace=%s&service=%s&updatePeriod=%s",
+		opts.URL,
+		url.QueryEscape(opts.Namespace),
+		url.QueryEscape(opts.Service),
+		url.QueryEscape(opts.UpdatePeriod.String()),
+	), bytes.NewReader(buf))
+	if err != nil {
+		return err
+	}
+
+	fmt.Fprintf(os.Stderr, "%s\n", req.URL.String())
+	fmt.Fprintf(os.Stdout, "Starting release of %s with an update period of %s... ", opts.Service, opts.UpdatePeriod.String())
+	begin := time.Now()
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	took := time.Since(begin).String()
+	switch resp.StatusCode {
+	case http.StatusOK:
+		fmt.Fprintf(os.Stdout, "success! (%s)\n", took)
+	default:
+		fmt.Fprintf(os.Stdout, "failed! %s (%s)\n", resp.Status, took)
+		io.Copy(os.Stdout, resp.Body)
+	}
+
 	return nil
 }
