6b310bb9efb826ec40de32955ce7ed47644e6ca8
Peter Bourgon <peter@bourgon.org>
2016-07-18 18:37:32 +0200

    Don't need entrypoint.sh any more

--- cmd/fluxd/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-
-/home/flux/fluxd \
-    --kubernetes \
-    --kubernetes-kubectl=/home/flux/kubectl \
-    --kubernetes-host="https://kubernetes" \
-    --kubernetes-certificate-authority=/var/run/secrets/kubernetes.io/serviceaccount/ca.crt \
-    --kubernetes-bearer-token="$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)"
