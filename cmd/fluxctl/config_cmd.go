1a6814a6a2b0874b67d34a106dacccbe78d095f4
Michael Bridgen <mikeb@squaremobius.net>
2016-07-18 17:05:48 +0100

    Add `config update` subcommand
    This finds all the versioned information in an RC config file and
    replaces it, printing out the result. `--verbose` makes it say what
    it's doing; `--output` writes to a file rather than stdout.

---
+++ cmd/fluxctl/config_cmd.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/spf13/cobra"
+)
+
+type configOpts struct {
+	*rootOpts
+}
+
+func newConfig(parent *rootOpts) *configOpts {
+	return &configOpts{rootOpts: parent}
+}
+
+func (opts *configOpts) Command() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "config",
+		Short: "Manipulate configuration files",
+	}
+	return cmd
+}
--- cmd/fluxctl/main.go
+++ cmd/fluxctl/main.go
@@ -9,8 +9,12 @@ func main() {
 	serviceList := newServiceList(service)
 	serviceImages := newServiceImages(service)
 	serviceRelease := newServiceRelease(service)
+
 	repo := newRepo(root)
 	repoImages := newRepoImages(repo)
+
+	config := newConfig(root)
+	configUpdate := newConfigUpdate(config)
 
 	rootCmd := root.Command()
 
@@ -18,13 +22,18 @@ func main() {
 	serviceListCmd := serviceList.Command()
 	serviceImagesCmd := serviceImages.Command()
 	serviceReleaseCmd := serviceRelease.Command()
+
 	repoCmd := repo.Command()
 	repoImagesCmd := repoImages.Command()
 
+	configCmd := config.Command()
+	configUpdateCmd := configUpdate.Command()
+
 	serviceCmd.AddCommand(serviceListCmd, serviceReleaseCmd, serviceImagesCmd)
 	repoCmd.AddCommand(repoImagesCmd)
+	configCmd.AddCommand(configUpdateCmd)
 
-	rootCmd.AddCommand(serviceCmd, repoCmd)
+	rootCmd.AddCommand(serviceCmd, repoCmd, configCmd)
 
 	if cmd, err := rootCmd.ExecuteC(); err != nil {
 		switch err.(type) {
