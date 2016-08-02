dbc5059277b64173eb6b90dc06f3d406c259552b
Paul Bellamy <paul.a.bellamy@gmail.com>
2016-08-02 13:31:48 +0100

    Review feedback

--- registry/registry_test.go
+++ registry/registry_test.go
@@ -2,113 +2,69 @@ package registry
 
 import "testing"
 
+var imageParsingExamples = map[string]Image{
+	"foo/bar": Image{
+		Registry: "",
+		Name:     "foo/bar",
+		Tag:      "",
+	},
+	"foo/bar:baz": Image{
+		Registry: "",
+		Name:     "foo/bar",
+		Tag:      "baz",
+	},
+	"reg:123/foo/bar:baz": Image{
+		Registry: "reg:123",
+		Name:     "foo/bar",
+		Tag:      "baz",
+	},
+	"docker-registry.domain.name:5000/repo/image1:ver": Image{
+		Registry: "docker-registry.domain.name:5000",
+		Name:     "repo/image1",
+		Tag:      "ver",
+	},
+	"shortreg/repo/image1": Image{
+		Registry: "shortreg",
+		Name:     "repo/image1",
+		Tag:      "",
+	},
+	"foo": Image{
+		Registry: "",
+		Name:     "foo",
+		Tag:      "",
+	},
+}
+
 func TestParseImage(t *testing.T) {
-	for _, input := range []struct {
-		in  string
-		out Image
-	}{
-		{
-			"foo/bar",
-			Image{Registry: "", Name: "foo/bar", Tag: ""},
-		},
-		{
-			"foo/bar:baz",
-			Image{Registry: "", Name: "foo/bar", Tag: "baz"},
-		},
-		{
-			"reg:123/foo/bar:baz",
-			Image{Registry: "reg:123", Name: "foo/bar", Tag: "baz"},
-		},
-		{
-			"docker-registry.domain.name:5000/repo/image1:ver",
-			Image{Registry: "docker-registry.domain.name:5000", Name: "repo/image1", Tag: "ver"},
-		},
-		{
-			"shortreg/repo/image1",
-			Image{Registry: "shortreg", Name: "repo/image1", Tag: ""},
-		},
-		{
-			"foo",
-			Image{Registry: "", Name: "foo", Tag: ""},
-		},
-	} {
-		out := ParseImage(input.in)
-		if out != input.out {
-			t.Fatalf("%s: %v != %v", input.in, out, input.out)
+	for in, want := range imageParsingExamples {
+		out := ParseImage(in)
+		if out != want {
+			t.Fatalf("%s: %v != %v", in, out, want)
 		}
 	}
 }
 
 func TestImageString(t *testing.T) {
-	for _, input := range []struct {
-		in  Image
-		out string
-	}{
-		{
-			Image{Registry: "", Name: "foo/bar", Tag: ""},
-			"foo/bar",
-		},
-		{
-			Image{Registry: "", Name: "foo/bar", Tag: "baz"},
-			"foo/bar:baz",
-		},
-		{
-			Image{Registry: "reg:123", Name: "foo/bar", Tag: "baz"},
-			"reg:123/foo/bar:baz",
-		},
-		{
-			Image{Registry: "docker-registry.domain.name:5000", Name: "repo/image1", Tag: "ver"},
-			"docker-registry.domain.name:5000/repo/image1:ver",
-		},
-		{
-			Image{Registry: "shortreg", Name: "repo/image1", Tag: ""},
-			"shortreg/repo/image1",
-		},
-		{
-			Image{Registry: "", Name: "foo", Tag: ""},
-			"foo",
-		},
-	} {
-		out := input.in.String()
-		if out != input.out {
-			t.Fatalf("Image{%q, %q, %q}.String(): %s != %s", input.in, out, input.out)
+	for want, in := range imageParsingExamples {
+		out := in.String()
+		if out != want {
+			t.Fatalf("%#v.String(): %s != %s", in, out, want)
 		}
 	}
 }
 
 func TestImageRepository(t *testing.T) {
-	for _, input := range []struct {
-		in  Image
-		out string
-	}{
-		{
-			Image{Registry: "", Name: "foo/bar", Tag: ""},
-			"foo/bar",
-		},
-		{
-			Image{Registry: "", Name: "foo/bar", Tag: "baz"},
-			"foo/bar",
-		},
-		{
-			Image{Registry: "reg:123", Name: "foo/bar", Tag: "baz"},
-			"reg:123/foo/bar",
-		},
-		{
-			Image{Registry: "docker-registry.domain.name:5000", Name: "repo/image1", Tag: "ver"},
-			"docker-registry.domain.name:5000/repo/image1",
-		},
-		{
-			Image{Registry: "shortreg", Name: "repo/image1", Tag: ""},
-			"shortreg/repo/image1",
-		},
-		{
-			Image{Registry: "", Name: "foo", Tag: ""},
-			"foo",
-		},
+	for in, want := range map[Image]string{
+		Image{Name: "foo/bar"}:                                                               "foo/bar",
+		Image{Name: "foo/bar", Tag: "baz"}:                                                   "foo/bar",
+		Image{Registry: "reg:123", Name: "foo/bar", Tag: "baz"}:                              "reg:123/foo/bar",
+		Image{Registry: "docker-registry.domain.name:5000", Name: "repo/image1", Tag: "ver"}: "docker-registry.domain.name:5000/repo/image1",
+		Image{Registry: "shortreg", Name: "repo/image1"}:                                     "shortreg/repo/image1",
+		Image{Name: "foo"}:                                                                   "foo",
 	} {
-		out := input.in.Repository()
-		if out != input.out {
-			t.Fatalf("Image{%q, %q, %q}.Repository(): %s != %s", input.in, out, input.out)
+		out := in.Repository()
+		if out != want {
+			t.Fatalf("%#v.Repository(): %s != %s", in, out, want)
 		}
 	}
 }
