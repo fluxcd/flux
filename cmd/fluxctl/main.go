6c3d92f94e631a13bf1512b5d3077b7f85041102
Peter Bourgon <peter@bourgon.org>
2016-08-17 17:54:00 +0100

    cmd/fluxctl: temp rm in advance of Michael's fixes

--- cmd/fluxctl/main.go
+++ cmd/fluxctl/main.go
@@ -1,15 +1,5 @@
 package main
 
-import "os"
-
 func main() {
-	rootCmd := newRoot().Command()
-	if cmd, err := rootCmd.ExecuteC(); err != nil {
-		switch err.(type) {
-		case usageError:
-			cmd.Println("")
-			cmd.Println(cmd.UsageString())
-		}
-		os.Exit(1)
-	}
+	// FIXME
 }
