e091cba2b760d3bd959617f06f96996cfafead2d
Peter Bourgon <peter@bourgon.org>
2016-08-17 18:22:44 +0100

    ListImages

--- server.go
+++ server.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/pkg/errors"
 
+	"github.com/weaveworks/fluxy/platform"
 	"github.com/weaveworks/fluxy/platform/kubernetes"
 	"github.com/weaveworks/fluxy/registry"
 )
@@ -21,11 +22,13 @@ func NewServer(platform *kubernetes.Cluster, registry *registry.Client) Service 
 	}
 }
 
-func (s *server) ListServices() ([]ServiceDescription, error) {
-	// This function is deliberately awkward, cobbled together from existing
-	// platform and registry APIs. I wanted to avoid changing those components
-	// until I got something working.
+// The server methods are deliberately awkward, cobbled together from existing
+// platform and registry APIs. I want to avoid changing those components until I
+// get something working. There's also a lot of code duplication here for the
+// same reason: let's not add abstraction until it's merged, or nearly so, and
+// it's clear where the abstraction should exist.
 
+func (s *server) ListServices() ([]ServiceDescription, error) {
 	var res []ServiceDescription
 	for _, namespace := range []string{
 		"default", // TODO(pb): s.platform.Namespaces()
@@ -87,8 +90,60 @@ func (s *server) ListServices() ([]ServiceDescription, error) {
 	return res, nil
 }
 
-func (s *server) ListImages(ServiceSpec) ([]ImageDescription, error) {
-	return nil, errors.New("ListImages not implemented by server")
+func (s *server) ListImages(spec ServiceSpec) ([]ImageDescription, error) {
+	m := map[string][]platform.Service{}
+	if spec == ServiceSpecAll {
+		for _, namespace := range []string{
+			"default", // TODO(pb): s.platform.Namespaces()
+		} {
+			services, err := s.platform.Services(namespace)
+			if err != nil {
+				return nil, errors.Wrapf(err, "fetching platform services for namespace %q", namespace)
+			}
+
+			m[namespace] = services
+		}
+	} else {
+		id, err := ParseServiceID(string(spec))
+		if err != nil {
+			return nil, errors.Wrap(err, "invalid service spec")
+		}
+
+		namespace, service := id.Components()
+		svc, err := s.platform.Service(namespace, service)
+		if err != nil {
+			return nil, errors.Wrapf(err, "fetching platform service %s", id)
+		}
+
+		m[namespace] = []platform.Service{svc}
+	}
+
+	var res []ImageDescription
+	for namespace, services := range m {
+		for _, service := range services {
+			containers, err := s.platform.ContainersFor(namespace, service.Name)
+			if err != nil {
+				return nil, errors.Wrapf(err, "fetching containers for %s/%s", namespace, service.Name)
+			}
+
+			for _, container := range containers {
+				imageID := ParseImageID(container.Image)
+				imageRepo, err := s.registry.GetRepository(imageID.Repository())
+				if err != nil {
+					return nil, errors.Wrapf(err, "fetching image repo for %s", imageID)
+				}
+
+				for _, image := range imageRepo.Images {
+					res = append(res, ImageDescription{
+						ID:        ParseImageID(image.String()),
+						CreatedAt: image.CreatedAt,
+					})
+				}
+			}
+		}
+	}
+
+	return res, nil
 }
 
 func (s *server) Release(ServiceSpec, ImageSpec, ReleaseKind) ([]ReleaseAction, error) {
