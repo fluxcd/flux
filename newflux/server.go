73bc5604045227f62f7ba4d5312d0b8903882d27
Peter Bourgon <peter@bourgon.org>
2016-08-17 17:54:30 +0100

    ListServices WIP

--- newflux/server.go
+++ newflux/server.go
@@ -1,15 +1,90 @@
 package flux
 
-import "errors"
+import (
+	"fmt"
 
-type server struct{}
+	"github.com/pkg/errors"
 
-func NewServer() Service {
-	return &server{}
+	"github.com/weaveworks/fluxy/platform/kubernetes"
+	"github.com/weaveworks/fluxy/registry"
+)
+
+type server struct {
+	platform *kubernetes.Cluster
+	registry *registry.Client
+}
+
+func NewServer(platform *kubernetes.Cluster, registry *registry.Client) Service {
+	return &server{
+		platform: platform,
+		registry: registry,
+	}
 }
 
 func (s *server) ListServices() ([]ServiceDescription, error) {
-	return nil, errors.New("ListServices not implemented by server")
+	// This function is deliberately awkward, cobbled together from existing
+	// platform and registry APIs. I wanted to avoid changing those components
+	// until I got something working.
+
+	var res []ServiceDescription
+	for _, namespace := range []string{
+		"default", // TODO(pb): s.platform.Namespaces()
+	} {
+		services, err := s.platform.Services(namespace)
+		if err != nil {
+			return nil, errors.Wrapf(err, "fetching platform services for namespace %q", namespace)
+		}
+
+		for _, service := range services {
+			idStr := fmt.Sprintf("%s/%s", namespace, service.Name)
+			id, err := ParseServiceID(idStr)
+			if err != nil {
+				return nil, errors.Wrapf(err, "parsing service ID %q", idStr)
+			}
+
+			// TODO(pb): containers should be returned as part of Services
+			containers, err := s.platform.ContainersFor(namespace, service.Name)
+			if err != nil {
+				return nil, errors.Wrapf(err, "fetching containers for %s/%s", namespace, service.Name)
+			}
+
+			var c []Container
+			for _, container := range containers {
+				imageID := ParseImageID(container.Image)
+				imageRepo, err := s.registry.GetRepository(imageID.Repository())
+				if err != nil {
+					return nil, errors.Wrapf(err, "fetching image repo for %s", imageID)
+				}
+
+				var (
+					current   ImageDescription
+					available []ImageDescription
+				)
+				for _, image := range imageRepo.Images {
+					description := ImageDescription{
+						ID:        ParseImageID(image.String()),
+						CreatedAt: image.CreatedAt,
+					}
+					available = append(available, description)
+					if image.String() == container.Image {
+						current = description
+					}
+				}
+				c = append(c, Container{
+					Name:      container.Name,
+					Current:   current,
+					Available: available,
+				})
+			}
+
+			res = append(res, ServiceDescription{
+				ID:         id,
+				Containers: c,
+			})
+		}
+	}
+
+	return res, nil
 }
 
 func (s *server) ListImages(ServiceSpec) ([]ImageDescription, error) {
