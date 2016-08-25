3e96688c7bfa1d3cc949660fd5968dee74a2f924
Michael Bridgen <mikeb@squaremobius.net>
2016-08-25 09:43:28 +0100

    Be more lenient with yaml indents
    It turns out not all indentation is significant in YAML; the test case
    in the commit has the same structure if you remove a level of indent
    from the containers items.

--- platform/kubernetes/update_test.go
+++ platform/kubernetes/update_test.go
@@ -3,16 +3,18 @@ package kubernetes
 import (
 	"bytes"
 	"fmt"
-	"io/ioutil"
 	"os"
 	"testing"
 )
 
 func TestTryUpdateCase1(t *testing.T) {
-	var trace bytes.Buffer
-	if err := tryUpdate(case1, "quay.io/weaveworks/pr-assigner:master-1234567", &trace, ioutil.Discard); err != nil {
+	var trace, out bytes.Buffer
+	if err := tryUpdate(case1, "quay.io/weaveworks/pr-assigner:master-1234567", &trace, &out); err != nil {
 		fmt.Fprintf(os.Stderr, "--- TRACE ---\n"+trace.String()+"\n---\n")
 		t.Error(err)
+	}
+	if string(out.Bytes()) != case1out {
+		t.Errorf("Did not get expected result:\n%s", string(out.Bytes()))
 	}
 }
 
@@ -34,6 +36,45 @@ spec:
       containers:
         - name: pr-assigner
           image: quay.io/weaveworks/pr-assigner:master-6f5e816
+          imagePullPolicy: IfNotPresent
+          args:
+            - --conf_path=/config/pr-assigner.json
+          env:
+            - name: GITHUB_TOKEN
+              valueFrom:
+                secretKeyRef:
+                  name: pr-assigner
+                  key: githubtoken
+          volumeMounts:
+            - name: config-volume
+              mountPath: /config
+      volumes:
+        - name: config-volume
+          configMap:
+            name: pr-assigner
+            items:
+              - key: conffile
+                path: pr-assigner.json
+`
+
+const case1out = `---
+apiVersion: extensions/v1beta1
+kind: Deployment
+metadata:
+  name: "pr-assigner"
+  namespace: extra
+spec:
+  replicas: 1
+  template:
+    metadata:
+      labels:
+        name: pr-assigner
+    spec:
+      imagePullSecrets:
+      - name: quay-secret
+      containers:
+        - name: pr-assigner
+          image: "quay.io/weaveworks/pr-assigner:master-1234567"
           imagePullPolicy: IfNotPresent
           args:
             - --conf_path=/config/pr-assigner.json
