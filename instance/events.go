e9cf765139add23cfa4fa5f27bb549fc3f97098b
Michael Bridgen <mikeb@squaremobius.net>
2016-10-19 12:11:22 +0100

    Whitespace: add open air between methods

--- instance/events.go
+++ instance/events.go
@@ -13,9 +13,11 @@ type EventReadWriter struct {
 func (rw EventReadWriter) LogEvent(namespace, service, msg string) error {
 	return rw.db.LogEvent(rw.inst, namespace, service, msg)
 }
+
 func (rw EventReadWriter) AllEvents() ([]history.Event, error) {
 	return rw.db.AllEvents(rw.inst)
 }
+
 func (rw EventReadWriter) EventsForService(namespace, service string) ([]history.Event, error) {
 	return rw.db.EventsForService(rw.inst, namespace, service)
 }
