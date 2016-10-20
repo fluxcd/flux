1fe9ec5fc8e353a35d56c878e0ed5b069e261278
Peter Bourgon <peter@bourgon.org>
2016-10-20 17:40:52 +0200

    with, not without, time zone

--- db/migrations/postgres/005_create_release_jobs_table.up.sql
+++ db/migrations/postgres/005_create_release_jobs_table.up.sql
@@ -1,13 +1,13 @@
 CREATE TABLE IF NOT EXISTS release_jobs (
     PRIMARY KEY (release_id),
-    release_id   UUID                         NOT NULL,
-    instance_id  varchar(255)                 NOT NULL,
-    spec         text                         NOT NULL,
-    submitted_at timestamp without time zone  NOT NULL,
-    claimed_at   timestamp without time zone,
-    heartbeat_at timestamp without time zone,
-    finished_at  timestamp without time zone,
-    log          text                         NOT NULL,
-    status       text                         NOT NULL,
+    release_id   UUID                      NOT NULL,
+    instance_id  varchar(255)              NOT NULL,
+    spec         text                      NOT NULL,
+    submitted_at timestamp with time zone  NOT NULL,
+    claimed_at   timestamp with time zone,
+    heartbeat_at timestamp with time zone,
+    finished_at  timestamp with time zone,
+    log          text                      NOT NULL,
+    status       text                      NOT NULL,
     success      boolean
 )
