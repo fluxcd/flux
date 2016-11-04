cf7b2a300a952a41e0fe1a86ed42f5a2c48e9c04
Michael Bridgen <mikeb@squaremobius.net>
2016-11-04 10:18:41 +0000

    Show the username for image reg creds
    A minor thing to help people orientate when trouble-shooting.

--- config.go
+++ config.go
@@ -1,5 +1,10 @@
 package flux
 
+import (
+	"encoding/base64"
+	"strings"
+)
+
 const secretReplacement = "******"
 
 // Instance configuration, mutated via `fluxctl config`. It can be
@@ -22,9 +27,11 @@ type RegistryConfig struct {
 	// Map of index host to Basic auth string (base64 encoded
 	// username:password), to make it easy to copypasta from docker
 	// config.
-	Auths map[string]struct {
-		Auth string `json:"auth" yaml:"auth"`
-	} `json:"auths" yaml:"auths"`
+	Auths map[string]Auth `json:"auths" yaml:"auths"`
+}
+
+type Auth struct {
+	Auth string `json:"auth" yaml:"auth"`
 }
 
 type InstanceConfig struct {
@@ -38,8 +45,19 @@ func (c InstanceConfig) HideSecrets() InstanceConfig {
 		c.Git.Key = secretReplacement
 	}
 	for host, auth := range c.Registry.Auths {
-		auth.Auth = secretReplacement
-		c.Registry.Auths[host] = auth
+		c.Registry.Auths[host] = auth.HidePassword()
 	}
 	return c
+}
+
+func (a Auth) HidePassword() Auth {
+	if a.Auth == "" {
+		return a
+	}
+	bytes, err := base64.StdEncoding.DecodeString(a.Auth)
+	if err != nil {
+		return Auth{secretReplacement}
+	}
+	parts := strings.SplitN(string(bytes), ":", 2)
+	return Auth{parts[0] + ":" + secretReplacement}
 }
