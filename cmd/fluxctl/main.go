e06b9603a81116d3c506129628754a97d455b897
Michael Bridgen <mikeb@squaremobius.net>
2016-12-02 16:10:29 +0000

    Detect when the client v service API is mismatched
    Getting a 404 result from the client usually means that there's a
    mismatch between the client and the service. This commit makes that
    situation detectable by returning a particular type for transport
    errors (as opposed to application errors), and treating 404 as a
    special case in the client.

--- cmd/fluxctl/main.go
+++ cmd/fluxctl/main.go
@@ -1,14 +1,36 @@
 package main
 
-import "os"
+import (
+	"os"
+	"strings"
+
+	"github.com/pkg/errors"
+
+	transport "github.com/weaveworks/flux/http"
+)
 
 func main() {
 	rootCmd := newRoot().Command()
 	if cmd, err := rootCmd.ExecuteC(); err != nil {
-		switch err.(type) {
+		err = errors.Cause(err)
+		switch err := err.(type) {
 		case usageError:
 			cmd.Println("")
 			cmd.Println(cmd.UsageString())
+		case *transport.APIError:
+			switch {
+			case err.IsMissing():
+				cmd.Println(strings.Join([]string{
+					"Error: Service API endpoint not found. This usually means that there is a mismatch between the client and the service. Please visit",
+					"    https://github.com/weaveworks/flux/releases",
+					"to download a new release of the client."}, "\n"))
+			default:
+				cmd.Println("Problem communicating with the service: ", err.Error())
+				cmd.Printf("Run '%v --help' for usage.\n", cmd.CommandPath())
+			}
+		default:
+			cmd.Println("Error: ", err.Error())
+			cmd.Printf("Run '%v --help' for usage.\n", cmd.CommandPath())
 		}
 		os.Exit(1)
 	}
