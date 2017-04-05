5efbc04fd97f75af8860483d2ec898ab941b967d
Adam Harrison <harrisonadamw@gmail.com>
2017-04-05 11:18:31 +0100

    Implement patch types and method

--- config.go
+++ config.go
@@ -101,3 +101,66 @@ func (g GitConfig) HideKey() GitConfig {
 	g.Key = string(ssh.MarshalAuthorizedKey(pubKey))
 	return g
 }
+
+type untypedConfig map[string]interface{}
+
+func (uc untypedConfig) toUnsafeInstanceConfig() (UnsafeInstanceConfig, error) {
+	bytes, err := json.Marshal(uc)
+	if err != nil {
+		return UnsafeInstanceConfig{}, err
+	}
+	var uic UnsafeInstanceConfig
+	if err := json.Unmarshal(bytes, &uic); err != nil {
+		return UnsafeInstanceConfig{}, err
+	}
+	return uic, nil
+}
+
+func (uic UnsafeInstanceConfig) toUntypedConfig() (untypedConfig, error) {
+	bytes, err := json.Marshal(uic)
+	if err != nil {
+		return nil, err
+	}
+	var uc untypedConfig
+	if err := json.Unmarshal(bytes, &uc); err != nil {
+		return nil, err
+	}
+	return uc, nil
+}
+
+type ConfigPatch map[string]interface{}
+
+func (uic UnsafeInstanceConfig) Patch(cp ConfigPatch) (UnsafeInstanceConfig, error) {
+	// Convert the strongly-typed config into an untyped form that's easier to patch
+	uc, err := uic.toUntypedConfig()
+	if err != nil {
+		return UnsafeInstanceConfig{}, err
+	}
+
+	applyPatch(uc, cp)
+
+	// If the modifications detailed by the patch have resulted in JSON which
+	// doesn't meet the config schema it will be caught here
+	return uc.toUnsafeInstanceConfig()
+}
+
+func applyPatch(uc untypedConfig, cp ConfigPatch) {
+	for key, value := range cp {
+		switch value := value.(type) {
+		case nil:
+			delete(uc, key)
+		case map[string]interface{}:
+			if uc[key] == nil {
+				uc[key] = make(map[string]interface{})
+			}
+			if uc, ok := uc[key].(map[string]interface{}); ok {
+				applyPatch(uc, value)
+			}
+		default:
+			// Remaining types; []interface{}, bool, float64 & string
+			// Note that we only support replacing arrays in their entirety
+			// as there is no way to address subelements for removal or update
+			uc[key] = value
+		}
+	}
+}
