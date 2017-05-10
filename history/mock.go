bd916b9824341723771d10acbfeca4d36b761d5a
Paul Bellamy <paul.a.bellamy@gmail.com>
2017-05-10 15:10:52 +0100

    Added test for daemon pullAndSync

--- history/mock.go
+++ history/mock.go
@@ -1,32 +1,56 @@
 package history
 
 import (
+	"database/sql"
+	"sync"
 	"time"
 
 	"github.com/weaveworks/flux"
 )
 
-type mock struct{}
-
-func NewMock() interface {
-	EventReader
-	EventWriter
-} {
-	return mock{}
+type Mock struct {
+	events []Event
+	sync.RWMutex
 }
 
-func (m mock) AllEvents(_ time.Time, _ int64) ([]Event, error) {
-	return nil, nil
+func NewMock() EventReadWriter {
+	return &Mock{}
 }
 
-func (m mock) EventsForService(_ flux.ServiceID, _ time.Time, _ int64) ([]Event, error) {
-	return nil, nil
+func (m *Mock) AllEvents(_ time.Time, _ int64) ([]Event, error) {
+	m.RLock()
+	defer m.RUnlock()
+	return m.events, nil
 }
 
-func (m mock) GetEvent(_ EventID) (Event, error) {
-	return Event{}, nil
+func (m *Mock) EventsForService(serviceID flux.ServiceID, _ time.Time, _ int64) ([]Event, error) {
+	m.RLock()
+	defer m.RUnlock()
+	var found []Event
+	for _, e := range m.events {
+		set := flux.ServiceIDSet{}
+		set.Add(e.ServiceIDs)
+		if set.Contains(serviceID) {
+			found = append(found, e)
+		}
+	}
+	return found, nil
 }
 
-func (m mock) LogEvent(_ Event) error {
+func (m *Mock) GetEvent(id EventID) (Event, error) {
+	m.RLock()
+	defer m.RUnlock()
+	for _, e := range m.events {
+		if e.ID == id {
+			return e, nil
+		}
+	}
+	return Event{}, sql.ErrNoRows
+}
+
+func (m *Mock) LogEvent(e Event) error {
+	m.Lock()
+	defer m.Unlock()
+	m.events = append(m.events, e)
 	return nil
 }
