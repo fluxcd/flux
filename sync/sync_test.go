eadd2ba5ad63ece8e752898033eacd77f09e3c0f
Ilya Dmitrichenko <errordeveloper@gmail.com>
2017-08-09 18:28:19 +0100

    Output combined YAML data when we fail to parse it

--- sync/sync_test.go
+++ sync/sync_test.go
@@ -149,6 +149,7 @@ func checkClusterMatchesFiles(t *testing.T, m cluster.Manifests, c cluster.Clust
 	}
 	resources, err := m.ParseManifests(conf)
 	if err != nil {
+		fmt.Printf("--- # begin contents:\n%s\n--- # end contents", conf)
 		t.Fatal(err)
 	}
 	files, err := m.LoadManifests(dir)
