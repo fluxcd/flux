01135ec8e3e94a6276c86458fcbe7016a8444ba6
Marcus Cobden <marcus@weave.works>
2017-10-12 14:34:32 +0100

    Add indexes for common queries

---
+++ db/migrations/postgres/012_query_index.up.sql
@@ -0,0 +1,6 @@
+CREATE INDEX events_instance_id_started_at_idx ON events USING btree(instance_id, started_at);
+
+CREATE INDEX jobs_instance_id_idx ON jobs USING btree(instance_id);
+CREATE INDEX jobs_busy_instance_id_idx ON jobs USING btree(instance_id) WHERE claimed_at IS NOT NULL AND finished_at IS NULL;
+CREATE INDEX jobs_queue_order_idx ON jobs USING btree((-1 * priority), scheduled_at, submitted_at) WHERE finished_at IS NULL AND claimed_at IS NULL;
+CREATE INDEX jobs_instance_id_unfinished_idx ON jobs USING btree(instance_id) WHERE finished_at IS NULL;
