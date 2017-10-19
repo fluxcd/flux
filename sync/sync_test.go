1fadf02b7f069a98e653394980d97a10234d6207
Tamara Kaufler <tamara@weave.works>
2017-10-19 22:21:39 +0100

    Puts mocks in a separate file

--- sync/sync_test.go
+++ sync/sync_test.go
@@ -10,8 +10,6 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/weaveworks/flux"
-
 	"github.com/go-kit/kit/log"
 
 	//	"github.com/weaveworks/flux"
@@ -22,84 +20,8 @@ import (
 	"github.com/weaveworks/flux/cluster/kubernetes/testfiles"
 	"github.com/weaveworks/flux/git"
 	"github.com/weaveworks/flux/git/gittest"
-	"github.com/weaveworks/flux/policy"
 	"github.com/weaveworks/flux/resource"
 )
-
-//------------------------------
-type rsc struct {
-	bytes []byte
-	Kind  string
-	Meta  struct {
-		Namespace string
-		Name      string
-	}
-}
-
-type rscIgnorePolicy struct {
-	bytes []byte
-	Kind  string
-	Meta  struct {
-		Namespace string
-		Name      string
-	}
-}
-
-func (rs rsc) Source() string {
-	return ""
-}
-
-func (rs rsc) Bytes() []byte {
-	return []byte{}
-}
-
-func (rs rsc) ResourceID() flux.ResourceID {
-	return flux.MakeResourceID(rs.Meta.Namespace, rs.Kind, rs.Meta.Name)
-}
-
-func (rs rsc) Policy() policy.Set {
-	p := policy.Set{}
-	return p
-}
-
-func (ri rscIgnorePolicy) Source() string {
-	return ""
-}
-
-func (ri rscIgnorePolicy) Bytes() []byte {
-	return []byte{}
-}
-
-func getBytes() []byte {
-	return []byte{}
-}
-
-func (ri rscIgnorePolicy) ResourceID() flux.ResourceID {
-	return flux.MakeResourceID(ri.Meta.Namespace, ri.Kind, ri.Meta.Name)
-}
-
-func (ri rscIgnorePolicy) Policy() policy.Set {
-	p := policy.Set{}
-	p[policy.Ignore] = "true"
-	fmt.Printf("!!! policy = %+v\n", p)
-	return p
-}
-
-func mockResourceWithoutIgnorePolicy(kind, namespace, name string) rsc {
-	r := rsc{Kind: kind}
-	r.Meta.Namespace = namespace
-	r.Meta.Name = name
-	return r
-}
-
-func mockResourceWithIgnorePolicy(kind, namespace, name string) rscIgnorePolicy {
-	ri := rscIgnorePolicy{Kind: kind}
-	ri.Meta.Namespace = namespace
-	ri.Meta.Name = name
-	return ri
-}
-
-//------------------------------
 
 func TestSync(t *testing.T) {
 	checkout, cleanup := setup(t)
