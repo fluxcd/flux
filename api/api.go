35539fd6d9c046d25f7f313813f504cfd01a86db
Sam Broughton <sam@weave.works>
2017-11-16 15:09:14 +0000

    Remove SyncNotify from api.Client

--- api/api.go
+++ api/api.go
@@ -17,7 +17,6 @@ type Client interface {
 	ListServices(ctx context.Context, namespace string) ([]flux.ControllerStatus, error)
 	ListImages(context.Context, update.ResourceSpec) ([]flux.ImageStatus, error)
 	UpdateImages(context.Context, update.ReleaseSpec, update.Cause) (job.ID, error)
-	SyncNotify(context.Context) error
 	JobStatus(context.Context, job.ID) (job.Status, error)
 	SyncStatus(ctx context.Context, ref string) ([]string, error)
 	UpdatePolicies(context.Context, policy.Updates, update.Cause) (job.ID, error)
