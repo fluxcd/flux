1d010ee687408442461c01173c479a2fe48afc80
Sam Broughton <sam@weave.works>
2017-12-17 19:52:31 +0200

    Remove mockClientSet noise
    We have this enormous block of nothingness all for satisying the requirements of
    a constructor we don't even need to use.

--- cluster/kubernetes/kubernetes_test.go
+++ cluster/kubernetes/kubernetes_test.go
@@ -8,173 +8,9 @@ import (
 	"testing"
 
 	"github.com/go-kit/kit/log"
-	discovery "k8s.io/client-go/discovery"
-	admissionregistrationv1alpha1 "k8s.io/client-go/kubernetes/typed/admissionregistration/v1alpha1"
-	appsv1beta1 "k8s.io/client-go/kubernetes/typed/apps/v1beta1"
-	authenticationv1 "k8s.io/client-go/kubernetes/typed/authentication/v1"
-	authenticationv1beta1 "k8s.io/client-go/kubernetes/typed/authentication/v1beta1"
-	authorizationv1 "k8s.io/client-go/kubernetes/typed/authorization/v1"
-	authorizationv1beta1 "k8s.io/client-go/kubernetes/typed/authorization/v1beta1"
-	autoscalingv1 "k8s.io/client-go/kubernetes/typed/autoscaling/v1"
-	autoscalingv2alpha1 "k8s.io/client-go/kubernetes/typed/autoscaling/v2alpha1"
-	batchv1 "k8s.io/client-go/kubernetes/typed/batch/v1"
-	batchv2alpha1 "k8s.io/client-go/kubernetes/typed/batch/v2alpha1"
-	certificatesv1beta1 "k8s.io/client-go/kubernetes/typed/certificates/v1beta1"
-	corev1 "k8s.io/client-go/kubernetes/typed/core/v1"
-	extensionsv1beta1 "k8s.io/client-go/kubernetes/typed/extensions/v1beta1"
-	networkingv1 "k8s.io/client-go/kubernetes/typed/networking/v1"
-	policyv1beta1 "k8s.io/client-go/kubernetes/typed/policy/v1beta1"
-	rbacv1alpha1 "k8s.io/client-go/kubernetes/typed/rbac/v1alpha1"
-	rbacv1beta1 "k8s.io/client-go/kubernetes/typed/rbac/v1beta1"
-	settingsv1alpha1 "k8s.io/client-go/kubernetes/typed/settings/v1alpha1"
-	storagev1 "k8s.io/client-go/kubernetes/typed/storage/v1"
-	storagev1beta1 "k8s.io/client-go/kubernetes/typed/storage/v1beta1"
 
 	"github.com/weaveworks/flux/cluster"
 )
-
-type mockClientset struct {
-}
-
-func (m *mockClientset) Discovery() discovery.DiscoveryInterface {
-	return nil
-}
-
-func (m *mockClientset) AdmissionregistrationV1alpha1() admissionregistrationv1alpha1.AdmissionregistrationV1alpha1Interface {
-	return nil
-}
-
-func (m *mockClientset) Admissionregistration() admissionregistrationv1alpha1.AdmissionregistrationV1alpha1Interface {
-	return nil
-}
-
-func (m *mockClientset) CoreV1() corev1.CoreV1Interface {
-	return nil
-}
-
-func (m *mockClientset) Core() corev1.CoreV1Interface {
-	return nil
-}
-
-func (m *mockClientset) AppsV1beta1() appsv1beta1.AppsV1beta1Interface {
-	return nil
-}
-
-func (m *mockClientset) Apps() appsv1beta1.AppsV1beta1Interface {
-	return nil
-}
-
-func (m *mockClientset) AuthenticationV1() authenticationv1.AuthenticationV1Interface {
-	return nil
-}
-
-func (m *mockClientset) Authentication() authenticationv1.AuthenticationV1Interface {
-	return nil
-}
-
-func (m *mockClientset) AuthenticationV1beta1() authenticationv1beta1.AuthenticationV1beta1Interface {
-	return nil
-}
-
-func (m *mockClientset) AuthorizationV1() authorizationv1.AuthorizationV1Interface {
-	return nil
-}
-
-func (m *mockClientset) Authorization() authorizationv1.AuthorizationV1Interface {
-	return nil
-}
-
-func (m *mockClientset) AuthorizationV1beta1() authorizationv1beta1.AuthorizationV1beta1Interface {
-	return nil
-}
-
-func (m *mockClientset) AutoscalingV1() autoscalingv1.AutoscalingV1Interface {
-	return nil
-}
-
-func (m *mockClientset) Autoscaling() autoscalingv1.AutoscalingV1Interface {
-	return nil
-}
-
-func (m *mockClientset) AutoscalingV2alpha1() autoscalingv2alpha1.AutoscalingV2alpha1Interface {
-	return nil
-}
-
-func (m *mockClientset) BatchV1() batchv1.BatchV1Interface {
-	return nil
-}
-
-func (m *mockClientset) Batch() batchv1.BatchV1Interface {
-	return nil
-}
-
-func (m *mockClientset) BatchV2alpha1() batchv2alpha1.BatchV2alpha1Interface {
-	return nil
-}
-
-func (m *mockClientset) CertificatesV1beta1() certificatesv1beta1.CertificatesV1beta1Interface {
-	return nil
-}
-
-func (m *mockClientset) Certificates() certificatesv1beta1.CertificatesV1beta1Interface {
-	return nil
-}
-
-func (m *mockClientset) ExtensionsV1beta1() extensionsv1beta1.ExtensionsV1beta1Interface {
-	return nil
-}
-
-func (m *mockClientset) Extensions() extensionsv1beta1.ExtensionsV1beta1Interface {
-	return nil
-}
-
-func (m *mockClientset) NetworkingV1() networkingv1.NetworkingV1Interface {
-	return nil
-}
-
-func (m *mockClientset) Networking() networkingv1.NetworkingV1Interface {
-	return nil
-}
-
-func (m *mockClientset) PolicyV1beta1() policyv1beta1.PolicyV1beta1Interface {
-	return nil
-}
-
-func (m *mockClientset) Policy() policyv1beta1.PolicyV1beta1Interface {
-	return nil
-}
-
-func (m *mockClientset) RbacV1beta1() rbacv1beta1.RbacV1beta1Interface {
-	return nil
-}
-
-func (m *mockClientset) Rbac() rbacv1beta1.RbacV1beta1Interface {
-	return nil
-}
-
-func (m *mockClientset) RbacV1alpha1() rbacv1alpha1.RbacV1alpha1Interface {
-	return nil
-}
-
-func (m *mockClientset) SettingsV1alpha1() settingsv1alpha1.SettingsV1alpha1Interface {
-	return nil
-}
-
-func (m *mockClientset) Settings() settingsv1alpha1.SettingsV1alpha1Interface {
-	return nil
-}
-
-func (m *mockClientset) StorageV1beta1() storagev1beta1.StorageV1beta1Interface {
-	return nil
-}
-
-func (m *mockClientset) StorageV1() storagev1.StorageV1Interface {
-	return nil
-}
-
-func (m *mockClientset) Storage() storagev1.StorageV1Interface {
-	return nil
-}
 
 type mockApplier struct {
 	commandRun bool
@@ -216,11 +52,10 @@ metadata:
 // ---
 
 func setup(t *testing.T) (*Cluster, *mockApplier) {
-	clientset := &mockClientset{}
 	applier := &mockApplier{}
-	kube, err := NewCluster(clientset, applier, nil, log.NewNopLogger())
-	if err != nil {
-		t.Fatal(err)
+	kube := &Cluster{
+		applier: applier,
+		logger:  log.NewNopLogger(),
 	}
 	return kube, applier
 }
