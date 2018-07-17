ce9578088f70a965e85841aeb935a064ffd63ad3
Michael Bridgen <mikeb@squaremobius.net>
2018-07-17 12:36:01 +0100

    Script to update gh-pages with new package

---
+++ bin/update-chart.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -v
+set -e
+set -u
+set -o pipefail
+
+scratch=$(mktemp -d -t flux-chart.XXXXXXXXXX)
+function cleanup {
+    rm -rf "$scratch"
+}
+trap cleanup EXIT
+
+REV="$1"
+if [ -z "$REV" ]; then
+    echo "Expected revision as sole argument" >&2
+    exit 1
+fi
+
+git clone -b "$REV" git@github.com:weaveworks/flux "$scratch"
+helm package "$scratch/chart/flux/"
+helm repo index . --url https://weaveworks.github.io/flux --merge index.yaml
