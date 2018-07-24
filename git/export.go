125a7e01f2ea1e0968146d52d72df42d0926f5d7
Michael Bridgen <mikeb@squaremobius.net>
2018-07-24 17:16:54 +0100

    Fix git.Export(sha1)
    `git clone -b ...` does not accept a revision -- it must be a branch
    or tag -- so we can't use that to export a repo at a revision, as we'd
    like to do for the helm operator's sync loop.
    
    Instead we'll have to clone without an argument, and checkout the
    desired revision.

--- git/export.go
+++ git/export.go
@@ -21,8 +21,11 @@ func (e *Export) Clean() {
 
 // Export creates a minimal clone of the repo, at the ref given.
 func (r *Repo) Export(ctx context.Context, ref string) (*Export, error) {
-	dir, err := r.workingClone(ctx, ref)
+	dir, err := r.workingClone(ctx, "")
 	if err != nil {
+		return nil, err
+	}
+	if err = checkout(ctx, dir, ref); err != nil {
 		return nil, err
 	}
 	return &Export{dir}, nil
