3766a1ab3b8154a2f2917ef6356b55ddaff868fb
Mans Matulewicz <mans.matulewicz@gmail.com>
2018-10-26 09:27:06 +0200

    making compiling work on raspberry pi's

---
+++ checkpoint/checkpoint_linux_arm.go
@@ -0,0 +1,23 @@
+package checkpoint
+
+import (
+	"syscall"
+)
+
+func getKernelVersion() string {
+	var uts syscall.Utsname
+	syscall.Uname(&uts)
+	return cstringToString(uts.Release[:])
+}
+
+func cstringToString(c []uint8) string {
+	s := make([]byte, len(c))
+	i := 0
+	for ; i < len(c); i++ {
+		if c[i] == 0 {
+			break
+		}
+		s[i] = uint8(c[i])
+	}
+	return string(s[:i])
+}
