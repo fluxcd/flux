a5ebc2125310ef2e1ac2c4ebd7ef57f571dc4289
Alfonso Acosta <fons@syntacticsugar.consulting>
2018-12-25 23:43:32 +0000

    show more context for errors from fluxctl
    Errors in fluxctl are unwrapped before printing since we want to
    format flux-specific errors specially, and they can come wrapped.
    
    However, the unwrapping can lead to losing context when printing
    non-flux-specific errors.
    
    For instance, after
    https://github.com/justinbarrick/go-k8s-portforward/pulls/3 we display
    the following when a kubeconfig file isn't found:
    
    Error: invalid configuration: no configuration has been provided
    Run 'fluxctl identity --help' for usage.
    
    Which is the cause of a much more helpful error when in full:
    
    Error: Could not load kubernetes configuration file: invalid configuration: no configuration has been provided
    Run 'fluxctl identity --help' for usage.
    
    This change removes the unwrapping for non-flux-specific errors.

--- cmd/fluxctl/main.go
+++ cmd/fluxctl/main.go
@@ -12,14 +12,13 @@ func run(args []string) int {
 	rootCmd := newRoot().Command()
 	rootCmd.SetArgs(args)
 	if cmd, err := rootCmd.ExecuteC(); err != nil {
-		err = errors.Cause(err)
-		switch err := err.(type) {
-		case *fluxerr.Error:
-			cmd.Println("== Error ==\n\n" + err.Help)
-		default:
-			cmd.Println("Error: " + err.Error())
-			cmd.Printf("Run '%v --help' for usage.\n", cmd.CommandPath())
+		// Format flux-specific errors. They can come wrapped,
+		// so we use the cause instead.
+		if cause, ok := errors.Cause(err).(*fluxerr.Error); ok {
+			cmd.Println("== Error ==\n\n" + cause.Help)
 		}
+		cmd.Println("Error: " + err.Error())
+		cmd.Printf("Run '%v --help' for usage.\n", cmd.CommandPath())
 		return 1
 	}
 	return 0
