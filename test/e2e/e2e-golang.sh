e4b7371eb2a4b26c8bbaaf187c4058caed3bfedf
stefanprodan <stefan.prodan@gmail.com>
2019-03-07 20:06:20 +0200

    Build and push on git tagging

--- test/e2e/e2e-golang.sh
+++ test/e2e/e2e-golang.sh
@@ -2,20 +2,18 @@
 
 set -o errexit
 
-curl -O https://storage.googleapis.com/golang/go1.11.4.linux-amd64.tar.gz
-tar -xvf go1.11.4.linux-amd64.tar.gz
+GO_VERSION=1.11.4
+
+echo ">>> Installing go ${GO_VERSION}"
+curl -O https://storage.googleapis.com/golang/go${GO_VERSION}.linux-amd64.tar.gz
+tar -xf go1.11.4.linux-amd64.tar.gz
 sudo rm -rf /usr/local/go
 sudo mv go /usr/local
 
 export GOPATH=$HOME/go
 export PATH=$PATH:/usr/local/go/bin:$GOPATH/bin
 
-go version
-
 mkdir -p $HOME/go/bin
 mkdir -p $HOME/go/src
 
-curl https://raw.githubusercontent.com/golang/dep/master/install.sh | sh
-dep ensure -vendor-only
-
-make all
+go version
