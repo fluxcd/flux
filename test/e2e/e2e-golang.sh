7abd74f50f9fe44b68da4e8faea7424fd9ec3d92
stefanprodan <stefan.prodan@gmail.com>
2019-03-07 15:49:01 +0200

    Remove previous go install

--- test/e2e/e2e-golang.sh
+++ test/e2e/e2e-golang.sh
@@ -4,6 +4,7 @@ set -o errexit
 
 curl -O https://storage.googleapis.com/golang/go1.11.4.linux-amd64.tar.gz
 tar -xvf go1.11.4.linux-amd64.tar.gz
+sudo rm -rf /usr/local/go
 sudo mv go /usr/local
 
 export GOPATH=$HOME/go
