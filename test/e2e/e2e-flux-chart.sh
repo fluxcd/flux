d54e6e5ad905098b3b831463c11ca940e8ca469d
stefanprodan <stefan.prodan@gmail.com>
2019-03-07 11:45:22 +0200

    Add known hosts to e2e setup

--- test/e2e/e2e-flux-chart.sh
+++ test/e2e/e2e-flux-chart.sh
@@ -5,18 +5,22 @@ set -o errexit
 REPO_ROOT=$(git rev-parse --show-toplevel)
 export KUBECONFIG="$(kind get kubeconfig-path --name="kind")"
 
+KNOWN_HOSTS=$(cat ${REPO_ROOT}/test/e2e/known_hosts)
+
 echo ">>> Installing Flux with Helm"
 helm install --name flux --wait \
 --namespace flux \
 --set git.url=ssh://git@gitsrv/git-server/repos/cluster.git \
 --set git.secretName=ssh-git \
+--set git.pollInterval=30s \
 --set helmOperator.create=true \
 --set helmOperator.createCRD=true \
 --set helmOperator.git.secretName=ssh-git \
 --set registry.excludeImage=* \
+--set-string ssh.known_hosts="${KNOWN_HOSTS}" \
 ${REPO_ROOT}/chart/flux
 
-sleep 60
+sleep 120
 
 echo ">>> Flux logs"
 kubectl -n flux logs deployment/flux
