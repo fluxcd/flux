a387d5a85eb98aaf0d8d853017a7b930a5db1c0d
Michael Bridgen <mikeb@squaremobius.net>
2019-04-26 15:54:54 +0100

    Sniff the OS and ARCH

--- test/e2e/e2e-echo.sh
+++ test/e2e/e2e-echo.sh
@@ -5,7 +5,10 @@
 set -o errexit
 
 source $(dirname $0)/e2e-paths.env
+source $(dirname $0)/machine.env
 
 echo REPO_ROOT=$REPO_ROOT
 echo BASE=$BASE
 echo GOBIN=$GOBIN
+echo OS=$OS
+echo ARCH=$ARCH
--- test/e2e/e2e-golang.sh
+++ test/e2e/e2e-golang.sh
@@ -3,14 +3,15 @@
 set -o errexit
 
 source $(dirname $0)/e2e-paths.env
+source $(dirname $0)/machine.env
 
 GO_VERSION=1.11.4
 
 echo ">>> Installing go ${GO_VERSION} to $GOBASE/go"
-if ! [ -f "go${GO_VERSION}.linux-amd64.tar.gz" ]; then
-    curl -O https://storage.googleapis.com/golang/go${GO_VERSION}.linux-amd64.tar.gz
+if ! [ -f "go${GO_VERSION}.${OS}-${ARCH}.tar.gz" ]; then
+    curl -O https://storage.googleapis.com/golang/go${GO_VERSION}.${OS}-${ARCH}.tar.gz
 fi
-tar -xf go1.11.4.linux-amd64.tar.gz
+tar -xf go1.11.4.${OS}-${ARCH}.tar.gz
 rm -rf $GOBASE/go
 mkdir -p $GOBASE
 mv go $GOBASE/
