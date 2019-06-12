197c49b45a0d95d2568aa46d5ba3a44f87553452
Daniel Holbach <daniel@weave.works>
2019-06-12 18:07:17 +0200

    remove documentation duplication

--- site/conf.py
+++ site/conf.py
@@ -18,7 +18,7 @@ sys.path.insert(0, os.path.abspath('.'))
 
 # -- Project information -----------------------------------------------------
 
-project = 'Flux documentation'
+project = 'Flux'
 copyright = '2019, Flux development team'
 author = 'Flux development team'
 
@@ -101,7 +101,7 @@ html_static_path = ['_static']
 # -- Options for HTMLHelp output ---------------------------------------------
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'Fluxdocumentationdoc'
+htmlhelp_basename = 'Fluxdoc'
 
 
 # -- Options for LaTeX output ------------------------------------------------
@@ -128,7 +128,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'Fluxdocumentation.tex', 'Flux documentation Documentation',
+    (master_doc, 'Flux.tex', 'Flux Documentation',
      'Flux development team', 'manual'),
 ]
 
@@ -138,7 +138,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'fluxdocumentation', 'Flux documentation Documentation',
+    (master_doc, 'flux', 'Flux Documentation',
      [author], 1)
 ]
 
@@ -149,8 +149,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'Fluxdocumentation', 'Flux documentation Documentation',
-     author, 'Fluxdocumentation', 'One line description of project.',
+    (master_doc, 'Flux', 'Flux Documentation',
+     author, 'Flux', 'One line description of project.',
      'Miscellaneous'),
 ]
 
