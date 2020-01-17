27838aa3e3910d238bab73717a8458f4d378c63d
Richard Case <198425+richardcase@users.noreply.github.com>
2020-01-17 16:46:40 +0000

    feat: removed specific oc target
    The `--oc` flag (and reated scc flags) have been removed. This change
    instead introduces the `--no-security-context` which when specified
    will stop any security context information being included in the
    deployments for flux/memcached. The security context information caused
    memcached to fail when running on OpenShift. A new service account
    has also been added for memcached.
    
    A separate documentation update will be needed detailing hot to add
    flux and memcached to a scc.
    
    Relates to: #2651

--- cmd/fluxctl/install_cmd.go
+++ cmd/fluxctl/install_cmd.go
@@ -51,9 +51,7 @@ fluxctl install --git-url 'git@github.com:<your username>/flux-get-started' | ku
 	cmd.Flags().BoolVar(&opts.RegistryScanning, "registry-scanning", true,
 		"scan container image registries to fill in the registry cache")
 	cmd.Flags().StringVarP(&opts.outputDir, "output-dir", "o", "", "a directory in which to write individual manifests, rather than printing to stdout")
-	cmd.Flags().BoolVar(&opts.OpenShiftTarget, "oc", false, "Install flux on OpenShift")
-	cmd.Flags().BoolVar(&opts.OpenShiftAddMemcachedSCC, "oc-add-memcached-scc", false, "Create a new SCC for memcached. Defaults to 'false'")
-	cmd.Flags().BoolVar(&opts.OpenShiftAddFluxSCC, "oc-add-flux-scc", false, "Create a new SCC for flux. Defaults to 'false'")
+	cmd.Flags().BoolVar(&opts.NoSecurityContext, "no-security-context", false, "Ensure that no security context information is added to the pod specs. Defaults to 'false'")
 
 	// Hide and deprecate "git-paths", which was wrongly introduced since its inconsistent with fluxd's git-path flag
 	cmd.Flags().MarkHidden("git-paths")
--- pkg/install/install_test.go
+++ pkg/install/install_test.go
@@ -1,18 +1,24 @@
 package install
 
 import (
+	"bytes"
+	"strings"
 	"testing"
 
 	"github.com/instrumenta/kubeval/kubeval"
 	"github.com/stretchr/testify/assert"
+
+	v1 "k8s.io/api/apps/v1"
+	"k8s.io/apimachinery/pkg/util/yaml"
 )
 
-func testFillInTemplates(t *testing.T, expectedManifestCount int, params TemplateParameters) {
+func testFillInTemplates(t *testing.T, expectedNoManifestst int, params TemplateParameters) map[string][]byte {
 	manifests, err := FillInTemplates(params)
 	assert.NoError(t, err)
-	assert.Len(t, manifests, expectedManifestCount)
+	assert.Len(t, manifests, expectedNoManifestst)
+
 	for fileName, contents := range manifests {
-		validationResults, err := kubeval.Validate(contents, config)
+		validationResults, err := kubeval.Validate(contents)
 		assert.NoError(t, err)
 		for _, result := range validationResults {
 			if len(result.Errors) > 0 {
@@ -24,10 +30,26 @@ func testFillInTemplates(t *testing.T, expectedManifestCount int, params Templat
 			}
 		}
 	}
+
+	return manifests
+}
+
+func testExtractDeployment(t *testing.T, data []byte) *v1.Deployment {
+	manifest := string(data)
+	manifest = strings.Replace(manifest, "---\n", "", -1)
+
+	decoder := yaml.NewYAMLOrJSONDecoder(bytes.NewBuffer([]byte(manifest)), 4096)
+	deployment := new(v1.Deployment)
+	err := decoder.Decode(deployment)
+	if err != nil {
+		t.Errorf("issue decoding memcache-dep.yaml into a deployment: %#v", err)
+	}
+
+	return deployment
 }
 
 func TestFillInTemplatesAllParameters(t *testing.T) {
-	testFillInTemplates(t, 5, TemplateParameters{
+	params := TemplateParameters{
 		GitURL:             "git@github.com:fluxcd/flux-get-started",
 		GitBranch:          "branch",
 		GitPaths:           []string{"dir1", "dir2"},
@@ -39,12 +61,22 @@ func TestFillInTemplatesAllParameters(t *testing.T) {
 		ManifestGeneration: true,
 		AdditionalFluxArgs: []string{"arg1=foo", "arg2=bar"},
 		RegistryScanning:   true,
-	})
+		NoSecurityContext:  false,
+	}
 
+	manifests := testFillInTemplates(t, 6, params)
+
+	memDeploy := manifests["memcache-dep.yaml"]
+	deployment := testExtractDeployment(t, memDeploy)
+
+	container := deployment.Spec.Template.Spec.Containers[0]
+	if container.SecurityContext == nil {
+		t.Error("security context not found when there should be one")
+	}
 }
 
 func TestFillInTemplatesMissingValues(t *testing.T) {
-	testFillInTemplates(t, 5, TemplateParameters{
+	testFillInTemplates(t, 6, TemplateParameters{
 		GitURL:           "git@github.com:fluxcd/flux-get-started",
 		GitBranch:        "branch",
 		GitPaths:         []string{},
@@ -68,4 +100,31 @@ func TestFillInTemplatesNoMemcached(t *testing.T) {
 		GitLabel:    "label",
 		GitReadOnly: false,
 	})
+}
+
+func TestFillInTemplatesNoSecurityContext(t *testing.T) {
+	params := TemplateParameters{
+		GitURL:             "git@github.com:fluxcd/flux-get-started",
+		GitBranch:          "branch",
+		GitPaths:           []string{"dir1", "dir2"},
+		GitLabel:           "label",
+		GitUser:            "User",
+		GitEmail:           "this.is@anemail.com",
+		Namespace:          "flux",
+		GitReadOnly:        false,
+		ManifestGeneration: true,
+		AdditionalFluxArgs: []string{"arg1=foo", "arg2=bar"},
+		RegistryScanning:   true,
+		NoSecurityContext:  true,
+	}
+
+	manifests := testFillInTemplates(t, 6, params)
+	memDeploy := manifests["memcache-dep.yaml"]
+
+	deployment := testExtractDeployment(t, memDeploy)
+
+	container := deployment.Spec.Template.Spec.Containers[0]
+	if container.SecurityContext != nil {
+		t.Errorf("security context found when there should be none: %#v", container.SecurityContext)
+	}
 }
