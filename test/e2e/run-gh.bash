1cd900a71706cbef735dd1c08a597a704c5665b6
stefanprodan <stefan.prodan@gmail.com>
2020-03-26 20:05:20 +0200

    ci: run e2e tests in GitHub Actions - use 4 Kubernetes Kind cluster for e2e tests - store Flux container image and cache dir in artifacts - download artifacts in e2e jobs to avoid rebuilding binaries and containers - cleanup artifacts daily

--- test/e2e/run-gh.bash
+++ test/e2e/run-gh.bash
@@ -16,16 +16,12 @@ KIND_CACHE_PATH="${CACHE_DIR}/kind-$KIND_VERSION"
 KIND_CLUSTER_PREFIX=flux-e2e
 BATS_EXTRA_ARGS=""
 
-sudo mv "$(go env GOPATH)/bin/fluxctl" "/usr/local/bin/fluxctl"
-
 # shellcheck disable=SC1090
 source "${E2E_DIR}/lib/defer.bash"
 trap run_deferred EXIT
 
 mkdir -p "${FLUX_ROOT_DIR}/cache"
 curl -sL "https://github.com/fluxcd/gitsrv/releases/download/${GITSRV_VERSION}/known_hosts.txt" > "${FLUX_ROOT_DIR}/cache/known_hosts"
-
-kind load docker-image 'docker.io/fluxcd/flux:latest'
 
 echo '>>> Running the tests'
 # Run all tests by default but let users specify which ones to run, e.g. with E2E_TESTS='11_*' make e2e
